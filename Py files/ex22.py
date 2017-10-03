627e82ff3741aaaad5a65dffb9ed3aeeb4a59a9f
Busterz <deddybuster@hotmail.com>
2017-10-04 01:08:42 +0800

    Added more files

---
+++ Py files/ex22.py
@@ -0,0 +1,1 @@
+# There is no practices for this exercise
---
+++ Py files/ex23.py
@@ -0,0 +1,9 @@
+# Some online repository centers:
+# Github.com
+# Bitbucket.org
+# Gitorious.org
+# Launchpad.net
+# Sourceforge.net
+# Freecode.com
+
+# This exercise encourages you to search for online python projects and read the code
