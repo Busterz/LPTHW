f1c5de865e00d0d069032e3404c8a2ccbf00c591
Busterz <deddybuster@hotmail.com>
2017-10-10 18:01:52 +0800

    Added more files

---
+++ Py files/ex46.py
@@ -0,0 +1,9 @@
+# Skeleton Directory. It will have your project layout, automated tests, modules, and install script.
+# Before you start, install the following Python pacakages:
+# 1. pip from http://pypi.python.org/pypi/pip
+# 2. distribute from http://pypi.python.org/pypi/distribute
+# 3. nose from http://pypi.python.org/pypi/nose
+# 4. virtualenv from http://pypi.python.org/pypi/virtualenv
+# Sometimes the Python installer does not add the C:\Python27\Script to the system PATH.
+# In that case, add it to the path just like C:\Python27
+# Make sure you read the documentation for distutils at http://docs.python.org/distutils/setupscript.html
